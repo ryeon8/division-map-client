92d28bc53f25eecfcb532c5261a7e8357cb7fe1a
ryeon <ryeon530@gmail.com>
2024-03-04 13:34:50 +0900

    ADD javadoc MODIFY 폴리라인 두께 및 투명도 수정 ADD 초기화 함수

--- public/js/kakaoMapApiFacade.js
+++ public/js/kakaoMapApiFacade.js
@@ -1,21 +1,35 @@
+/**
+ * kakao map api 호출부 facade.
+ * 
+ * @author r3n
+ * @since 2024. 03. 04.
+ */
+
+let objectsOnMap = []
+
+const remove = {
+  all: () => {
+    objectsOnMap.forEach(e => e.setMap(null));
+    objectsOnMap = [];
+  },
+  line: () => {
+    console.log('remove line')
+  }
+}
+
 const draw = {
   line: (map, latLngList) => {
     const path = latLngList.map(latLngPair => new kakao.maps.LatLng(latLngPair[1], latLngPair[0]));
     const polyline = new kakao.maps.Polyline({
       path: path,
-      strokeWeight: 5,
+      strokeWeight: 3,
       strokeColor: 'blue',
-      strokeOpacity: 0.7,
+      strokeOpacity: 1,
       strokeStyle: 'solid'
     });
 
     polyline.setMap(map);
-  }
-}
-
-const remove = {
-  line: () => {
-    console.log('remove line')
+    objectsOnMap.push(polyline);
   }
 }
 
